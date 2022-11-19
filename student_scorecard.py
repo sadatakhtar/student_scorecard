da0e55825ff5238e9c5c5f43372e0bb112493e4b
sadat <shadowak47@yahoo.com>
2022-11-19 12:41:47 +0000

    Created 2 functions, 1 to greet user and 2nd to get user inputs

--- student_scorecard.py
+++ student_scorecard.py
@@ -1,5 +1,34 @@
 """
 A simple program to capture students name, score and grade
 """
-def aaa():
-    pass
+
+# Declare variables
+name_list = [''] * 100
+score_list = [0] * 100
+grade_list = [''] * 100
+
+
+def greeting():
+    # A Greeting function
+    print('*' * 42)
+    print('*' * 3 + ' Welcome to pythons student tracker ' + '*' * 3)
+    print('*' * 42)
+
+def user_inputs():
+    student_name = input("Enter students name: ")
+    student_score = int(input("Enter students score:"))
+    name_list.append(student_name)
+    score_list.append(student_score)
+    if student_score > 70:
+        grade_list.append('Distinction')
+    elif student_score > 50 and student_score < 70:
+        grade_list.append('Merit')
+    elif student_score > 30 and student_score < 50:
+        grade_list.append('Pass')
+    else:
+        grade_list.append('Fail')
+    return student_name, student_score
+
+
+
+
